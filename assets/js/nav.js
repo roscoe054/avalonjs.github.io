522f563516d0f95e713953c668cbc2627a9cf036
yuhao.ju <heiwu805@hotmail.com>
2016-01-14 11:53:44 +0800

    二级目录

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -6,33 +6,24 @@
         $id: "nav",
         aname: "",
         bname: "",
-        aclick: function (href, e) {
-            // vm.aname = href
-            // $('html,body').animate({scrollTop: 0}, 500)
+        aclick: function (href, sub, name) {
+            avalon.vmodels.tutorial.currentName = name
         },
-        bclick: function (href, e) {
-            vm.bname = href
-            //如果从一个子页跳转到另一个子页
-            if (window.activeArray.length <= 1) {
+        bclick: function (e, href) {
+            e.preventDefault()
+            var d = document.getElementById(href)
+            if(d){
+                var top = avalon(d).offset().top - 50
                 $('html,body').animate({
-                    scrollTop: 0
+                    scrollTop: Math.max(0, top)
                 }, 500)
-            } else {
-                //如果是一个子页间的锚点的移动
-                window.activeArray.some(function (el) {
-                    if (el.id === href) {
-                        $('html,body').animate({
-                            scrollTop: Math.max(0, el.top - bannerHeight)
-                        }, 500)
-                    }
-                })
             }
         },
         array: [
             {
-                href: "tutorial/home/",
+                href: "tutorial/home",
                 name: "介绍",
-                sub: [
+                anchors: [
                     {
                         href: "home-download",
                         name: "下载"
@@ -54,145 +45,145 @@
             },
 
             {
-                href: "tutorial/concepts/",
+                href: "tutorial/concepts",
                 name: "核心概念",
                 sub: [
                     {
-                        href: "tutorial/concepts/vmodel.html",
+                        href: "tutorial/concepts/vmodel",
                         name: "视图模型"
                     },
                     {
-                        href: "tutorial/concepts/$model.html",
+                        href: "tutorial/concepts/$model",
                         name: "数据模型"
                     },
                     {
-                        href: "tutorial/concepts/observable.html",
+                        href: "tutorial/concepts/observable",
                         name: "监控属性"
                     },
                     {
-                        href: "tutorial/concepts/collection.html",
+                        href: "tutorial/concepts/collection",
                         name: "监控数组"
                     },
                     {
-                        href: "tutorial/concepts/computed.html",
+                        href: "tutorial/concepts/computed",
                         name: "计算属性"
                     },
                     {
-                        href: "tutorial/concepts/$watch.html",
+                        href: "tutorial/concepts/$watch",
                         name: "$watch方法"
                     },
                     {
-                        href: "tutorial/concepts/$unwatch.html",
+                        href: "tutorial/concepts/$unwatch",
                         name: "$unwatch方法"
                     },
                     {
-                        href: "tutorial/concepts/unobservable.html",
+                        href: "tutorial/concepts/unobservable",
                         name: "非监控属性"
                     }
                 ]
             },
             {
-                href: "tutorial/directives/",
+                href: "tutorial/directives",
                 name: "指令",
                 sub: [
                     {
-                        href: "tutorial/directives/attr.html",
+                        href: "tutorial/directives/attr",
                         name: "ms-attr"
                     },
                     {
-                        href: "tutorial/directives/class.html",
+                        href: "tutorial/directives/class",
                         name: "ms-active"
                     },
                     {
-                        href: "tutorial/directives/controller.html",
+                        href: "tutorial/directives/controller",
                         name: "ms-controller"
                     },
                     {
-                        href: "tutorial/directives/class.html",
+                        href: "tutorial/directives/class",
                         name: "ms-class"
                     },
                     {
-                        href: "tutorial/directives/css.html",
+                        href: "tutorial/directives/css",
                         name: "ms-css"
                     },
                     {
-                        href: "tutorial/directives/data.html",
+                        href: "tutorial/directives/data",
                         name: "ms-data"
                     },
                     {
-                        href: "tutorial/directives/duplex.html",
+                        href: "tutorial/directives/duplex",
                         name: "ms-duplex"
                     },
                     {
-                        href: "tutorial/directives/repeat.html",
+                        href: "tutorial/directives/repeat",
                         name: "ms-each"
                     },
                     {
-                        href: "tutorial/directives/effect.html",
+                        href: "tutorial/directives/effect",
                         name: "ms-effect"
                     },
                     {
-                        href: "tutorial/directives/text.html",
+                        href: "tutorial/directives/text",
                         name: "ms-html"
                     },
                     {
-                        href: "tutorial/directives/class.html",
+                        href: "tutorial/directives/class",
                         name: "ms-hover"
                     },
                     {
-                        href: "tutorial/directives/if.html",
+                        href: "tutorial/directives/if",
                         name: "ms-if"
                     },
                     {
-                        href: "tutorial/directives/controller.html",
+                        href: "tutorial/directives/controller",
                         name: "ms-important"
                     },
                     {
-                        href: "tutorial/directives/include.html",
+                        href: "tutorial/directives/include",
                         name: "ms-include"
                     },
                     {
-                        href: "tutorial/directives/on.html",
+                        href: "tutorial/directives/on",
                         name: "ms-on"
                     },
                     {
-                        href: "tutorial/directives/repeat.html",
+                        href: "tutorial/directives/repeat",
                         name: "ms-repeat"
                     },
                     {
-                        href: "tutorial/directives/controller.html",
+                        href: "tutorial/directives/controller",
                         name: "ms-skip"
                     },
                     {
-                        href: "tutorial/directives/text.html",
+                        href: "tutorial/directives/text",
                         name: "ms-text"
                     },
                     {
-                        href: "tutorial/directives/visible.html",
+                        href: "tutorial/directives/visible",
                         name: "ms-visible"
                     },
                     {
-                        href: "tutorial/directives/repeat.html",
+                        href: "tutorial/directives/repeat",
                         name: "ms-with"
                     },
                     {
-                        href: "tutorial/directives/widget.html",
+                        href: "tutorial/directives/widget",
                         name: "ms-widget"
                     },
                      {
-                        href: "tutorial/directives/custom.html",
+                        href: "tutorial/directives/custom",
                         name: "自定义标签"
                     },
                      {
-                        href: "tutorial/directives/i20n.html",
+                        href: "tutorial/directives/i20n",
                         name: "国际化"
                     }
                 ]
             },
             {
-                href: "tutorial/filters/",
+                href: "tutorial/filters",
                 name: "过滤器",
-                sub: [
+                anchors: [
                     {
                         href: "camelize",
                         name: "camelize"
@@ -236,9 +227,9 @@
                 ]
             },
             {
-                href: "tutorial/interceptors/",
+                href: "tutorial/interceptors",
                 name: "拦截器",
-                sub: [
+                anchors: [
                     {
                         href: "ms-duplex-string",
                         name: "ms-duplex-string"
@@ -258,9 +249,9 @@
                 ]
             },
             {
-                href: "tutorial/component/",
+                href: "tutorial/component",
                 name: "组件",
-                sub: [
+                anchors: [
                     {
                         href: "callbacks",
                         name: "所有回调的执行顺序"
@@ -292,47 +283,47 @@
                 ]
             },
             {
-                href: "tutorial/callbacks/",
+                href: "tutorial/callbacks",
                 name: "回调"
             },
             {
-                href: "tutorial/configuration/",
+                href: "tutorial/configuration",
                 name: "配置"
             },
             {
-                href: "tutorial/engineering/",
+                href: "tutorial/engineering",
                 name: "工程化",
                 sub:[
                   {
-                      href: "tutorial/engineering/jquery.html",
+                      href: "tutorial/engineering/jquery",
                       name: "与jquery混用"
                   },
                   {
-                      href: "tutorial/engineering/weixin.html",
+                      href: "tutorial/engineering/weixin",
                       name: "在微信中使用"
                   },
                    {
-                      href: "tutorial/engineering/mobile.html",
+                      href: "tutorial/engineering/mobile",
                       name: "在移动端使用"
                   },
                   {
-                      href: "tutorial/engineering/loader.html",
+                      href: "tutorial/engineering/loader",
                       name: "与requirejs混用"
                   },
                    {
-                      href: "tutorial/engineering/webpack.html",
+                      href: "tutorial/engineering/webpack",
                       name: "使用webpack打包"
                   },
                    {
-                      href: "tutorial/engineering/project.html",
+                      href: "tutorial/engineering/project",
                       name: "webpack+avalon"
                   },
                    {
-                      href: "tutorial/engineering/fis.html",
+                      href: "tutorial/engineering/fis",
                       name: "fis3+avalon"
                   },
                     {
-                      href: "tutorial/engineering/seo.html",
+                      href: "tutorial/engineering/seo",
                       name: "SEO"
                   }
                 ]
@@ -341,7 +332,7 @@
 
     })
 
-    avalon.scan(0, vm)
+    // avalon.scan(0, vm)
     $(".navbar-nav a").click(function (e) {
         var hash = this.getAttribute("href", 2)
         if (hash.indexOf("/") > 0) {
